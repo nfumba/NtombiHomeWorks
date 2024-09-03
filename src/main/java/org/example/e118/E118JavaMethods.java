fb8bde545e3b319fcfdcb4d80c08f138109e2bf1
nfumba <fikile.fumba@gmail.com>
2024-09-03 06:40:52 +0200

    Added homework 118 and 119

--- src/main/java/org/example/e118/E118JavaMethods.java
+++ src/main/java/org/example/e118/E118JavaMethods.java
@@ -3,6 +3,17 @@ package org.example.e118;
 public class E118JavaMethods {
 
     public static void main(String[] args) {
-        // Students will write the code here.
+        System.out.println(spaceOut("Technology"));
     }
-}
+        // Students will write the code here.
+        public static String spaceOut(String s) {
+            String result = "";
+            for (int i = 0; i < s.length(); i++) {
+                result += s.charAt(i) + " ";
+            }
+            return result;
+        }
+
+
+    }
+
--- src/main/java/org/example/e119/E119JavaMethods.java
+++ src/main/java/org/example/e119/E119JavaMethods.java
@@ -2,7 +2,14 @@ package org.example.e119;
 
 public class E119JavaMethods {
     public static void main(String[] args) {
-        // Students will write the code here.
+        System.out.println(censorLetter("computer science",'e'));
+
     }
+    // Students will write the code here.
+    public static String censorLetter(String s, char c) {
+        return s.replace(String.valueOf(c), "*");
+    }
+
+
 
 }
