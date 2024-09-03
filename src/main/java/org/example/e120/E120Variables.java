d8487683a2dedf531eade858cc57f9edc1c5e427
nfumba <fikile.fumba@gmail.com>
2024-09-03 21:49:05 +0200

    Added homework 118 and 119

--- src/main/java/org/example/e120/E120Variables.java
+++ src/main/java/org/example/e120/E120Variables.java
@@ -2,9 +2,26 @@ package org.example.e120;
 
 public class E120Variables {
     // Declare 3 instance variables: year, school name, and batch number
+    public static class Student {
 
-    public static void main(String[] args) {
-        // Access variables from the main method and assign specific values to them
-        // Print values of your variables in the specified format
+        int year;
+        String schoolName;
+        int batchNumber;
+
+
+        public static void main(String[] args) {
+            Student student = new Student();
+
+            // Access variables from the main method and assign specific values to them
+            student.year = 2024;
+            student.schoolName = "Syntax";
+            student.batchNumber = 6;
+
+
+            // Print values of your variables in the specified format
+            System.out.print("I am a student of " + student.batchNumber + " ");
+            System.out.print("studying at " + student.schoolName + " ");
+            System.out.print("in the year of" + student.year +" ");
+        }
     }
 }
--- src/main/java/org/example/e121/E121Variables.java
+++ src/main/java/org/example/e121/E121Variables.java
@@ -2,12 +2,40 @@ package org.example.e121;
 
 public class E121Variables {
     // Declare 3 instance variables: integer, double, and char
+    public static class MyClass {
+        int intValue;
+        double doubleValue;
+        char charValue;
 
-    public static void main(String[] args) {
-        // Create 2 instances of the class
-        // Assign values to variables for the first instance
-        // Print values of the first instance
-        // Assign values to variables for the second instance
-        // Print values of the second instance
+
+        public static void main(String[] args) {
+            // Create 2 instances of the class
+            // Create 2 instances of the class
+            MyClass instance1 = new MyClass();
+            MyClass instance2 = new MyClass();
+
+            // Assign values to variables for the first instance
+            instance1.intValue = 10;
+            instance1.doubleValue = 20.5;
+            instance1.charValue = 'A';
+
+            // Print values of the first instance
+            System.out.println("Instance 1:");
+            System.out.println("Int Value: " + instance1.intValue);
+            System.out.println("Double Value: " + instance1.doubleValue);
+            System.out.println("Char Value: " + instance1.charValue);
+
+            // Assign values to variables for the second instance
+            instance2.intValue = 30;
+            instance2.doubleValue = 40.75;
+            instance2.charValue = 'B';
+
+            // Print values of the second instance
+            System.out.println("\nInstance 2:");
+            System.out.println("Int Value: " + instance2.intValue);
+            System.out.println("Double Value: " + instance2.doubleValue);
+            System.out.println("Char Value: " + instance2.charValue);
+
+        }
     }
 }
